ae94d4954498108aca70171d5dc4126341f24adf
Tim232 <64291996+Tim232@users.noreply.github.com>
2020-11-04 09:18:03 +0900

    Load Cogs

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from discord.ext import commands
+import os
 bot=commands.Bot(command_prefix='!")
+for file in os.listidr('cogs'):
+    if file.endswith(/.py'):
+        bot.load_extension('cogs.'+file)
 bot.run('token')
